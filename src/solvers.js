4a7bc4c4cbbd0f4fc1fcf43ec448e17e4a66c762
Brian Nguyen <brianacnguyen@gmail.com>
2015-10-15 10:53:50 -0700

    solved first solver

--- src/solvers.js
+++ src/solvers.js
@@ -17,37 +17,30 @@
 // find queens = similar but with conditionals of rows and collumns and diagonals 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; 
   var newBoard = new Board({n: n});
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var rowIndex = 0;
+  var columnIndex = n - 1;
+  //for loop
+  for (var i = 0; i < n; i++) {
+    newBoard.togglePiece(rowIndex,columnIndex);
+    rowIndex += 1; 
+    columnIndex -= 1;
+  }
+  /*
+  [0,0,1]
+  [0,1,0]
+  [1,0,0]
+  */
+  var totalRows = newBoard.rows();
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(newBoard));
+  return totalRows;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-  var test = function(boardSize) {
-    var outcomes = []; 
-    var plays = ['rock', 'paper', 'scissors'];
-    var combos = function(roundsToGo, playedSoFar) {
-      if (roundsToGo === 0) {
-        outcomes.push (playedSoFar); 
-        return;
-      }
-      for (var i = 0; i < plays.length; i++) {
-        var currentPlay = plays[i];
-        combos(roundsToGo - 1, playedSoFar.concat(currentPlay));
-      };
-    }
-    combos(boardSize, []);
-    return outcomes;
-  };
-  test(n);
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
-};
+}
 
 
 
